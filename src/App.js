0f7bf2ae74147fde80461087fd5812b690041e7d
sahariarahmmadshakil <info.sashakil@gmail.com>
2022-04-07 02:51:28 +0600

    added some secion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import About from './components/About/About';
+import Friends from './components/Friends/Friends';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 function App() {
-const departments =['cse','eee','law','mechanical','cicil','textile'];
 
   return (
     <div className="App">
-      
+      <Home></Home>
+      <Header></Header>
+      <Friends></Friends>
+      <About></About>
     </div>
   );
 }
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Header = () => {
+const About = () => {
     return (
         <div>
-            <h1>This is header</h1>
+            <h3>This is About</h3>
         </div>
     );
 };
 
-export default Header;
+export default About;
