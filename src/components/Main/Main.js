01d7f7446328e351b6bac9222edfb9895eaf8041
sahariarahmmadshakil <info.sashakil@gmail.com>
2022-04-09 03:02:29 +0600

    added search filter

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,15 +6,31 @@ import './Main.css'
 
 
 const Main = () => {
-    const [volenters, setVolenters]=useVolenter();
+    const [volenters, setVolenters] = useVolenter();
+    const [searchText, setSearchText]=useState([]);
+
+    const searchFIlter= event=>{
+        const text=event.target.value;
+        const match=volenters.filter(v=>v.title.includes(text));
+        setSearchText(match);
+    }
+
     return (
-        <div className='activity-container'>            
-            {
-                volenters.map(activity=><Activity
-                key={activity.id}
-                activity={activity}
-                ></Activity>)
-            }
+        <div>
+            <h1>Search From here</h1>
+            <input onChange={searchFIlter} type="text" placeholder='Search Here' /><br />
+            <br />
+            <div className='activity-container'>
+
+
+
+                {
+                    searchText.map(activity => <Activity
+                        key={activity.id}
+                        activity={activity}
+                    ></Activity>)
+                }
+            </div>
         </div>
     );
 };
